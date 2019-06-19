33c253f4c5ced300eb7175756d9e743efd27e31b
Kyle Barton <bartokyl000@gmail.com>
2019-06-19 18:42:35 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -28,3 +28,11 @@ if valid_move?(board,index)
   display_board(board)
 end
 
+def valid_move?(board, index)
+
+  if index.between?(0,8) && !position_taken?(board, index)
+      puts 'this is a valid move'
+    return true
+  else
+   return false
+  end
