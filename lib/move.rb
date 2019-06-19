1029eadb8d3d17f33f45c9b1a763562c7ffe72fc
Kyle Barton <bartokyl000@gmail.com>
2019-06-19 18:43:38 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,37 +6,3 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(move)
-   index = move.to_i - 1
-   index
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  token = current_player(board)
-
-if valid_move?(board,index)
-    puts 'valid move'
-    move(board, index, token)
-    display_board(board)
-   else
-    puts 'try again'
-    turn(board)
-  end
-  display_board(board)
-end
-
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-def valid_move?(board, index)
-
-  if index.between?(0,8) && !position_taken?(board, index)
-      puts 'this is a valid move'
-    return true
-  else
-   return false
-  end
